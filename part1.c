3e0a8e91d4d79fa69fc78dde11515361af7a0745
Augustovb <augustov.b@hotmail.com>
2014-05-14 11:00:42 -0300

    Aca simplemente son comentarios para probar el git.

---
+++ part1.c
@@ -0,0 +1,6 @@
+ //Aca hay info sobre como hacer esto. Commit es para volver a subir a la nube. clone se sube
+//
+// con git status ahi te manda en verde lo que se paso a la nube local
+
+// git add . involucras a toda la carpeta
+
